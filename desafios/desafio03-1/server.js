2160061909b4ed5958cc1868e3abf9311370e747
Mailson Ferreira <mds_mfl@hotmail.com>
2020-07-28 12:06:19 -0300

    atividade3

--- desafios/desafio03-1/server.js
+++ desafios/desafio03-1/server.js
@@ -1,10 +1,33 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
 
-server.get("/", function(req, res){
-    return res.send('Hello, oi oi')
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server
 })
 
-server.listen(3000, function(){
+server.get("/", function (req, res) {
+    return res.render("courses")
+})
+
+server.get("/about", function (req, res) {
+    return res.render("about")
+})
+
+
+server.use(function(req, res) {
+    res.status(404).render("not-found");
+  })
+
+
+
+
+
+server.listen(3000, function () {
     console.log('SERVER RODANDO')
 })
