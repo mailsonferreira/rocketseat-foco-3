704adf914d95c0d17ed2d7fb324bd77bc5fcc48e
Mailson Ferreira <mds_mfl@hotmail.com>
2020-07-28 18:23:00 -0300

    add ajusts

--- curso/modulo03/server.js
+++ curso/modulo03/server.js
@@ -9,11 +9,25 @@ server.use(express.static('public')) // arquivo css
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express:server,
+    autoescape: false
 })
 
 server.get("/", function(req, res){
-    return res.render("about")
+
+    const about = {
+        avatar_url: "https://avatars2.githubusercontent.com/u/60264180?s=400&u=03c77a61074f43b7a2c2df26913da50da89f2875&v=4",
+        name: "Mailson Ferreira",
+        role: "Aluno - Rocketseat",
+        description: 'Cursando o bootcamp LaunchBase com Mayk Brito da <a href="https://rocketseat.com.br/" target="_blank">  Rocketseat ',
+        links: [
+            {name: "Github", url: "https://github.com/mailsonferreira"},
+            {name: "Linkedin", url: "https://www.linkedin.com/in/mailson-ferreira/"},
+            {name: "Instagram", url: "https://www.instagram.com/mailsonferreira/"}
+        ]
+    }
+
+    return res.render("about", { about})
 })
 
 server.get("/portfolio", function(req, res){
